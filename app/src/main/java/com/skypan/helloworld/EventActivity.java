1318950510522876172867a56562b55abddcf5fd
chlbupt <1032326058@qq.com>
2021-10-01 15:01:44 +0800

    基于回调的事件处理机制

--- app/src/main/java/com/skypan/helloworld/EventActivity.java
+++ app/src/main/java/com/skypan/helloworld/EventActivity.java
@@ -3,12 +3,14 @@ package com.skypan.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 public class EventActivity extends AppCompatActivity{
-    private Button mBtnClick;
+    private Button mBtnClick, mBtnMy;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +18,19 @@ public class EventActivity extends AppCompatActivity{
         setContentView(R.layout.activity_event);
 
         mBtnClick = findViewById(R.id.btn_click);
+        mBtnMy = findViewById(R.id.btn_my);
+
+        mBtnMy.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                switch (motionEvent.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        Log.d("Listener", "---Listener---");
+                        break;
+                }
+                return false;
+            }
+        });
         // 同一事件源绑定多个相同的事件监听器，以最后一个为准
         // 匿名内部类
         mBtnClick.setOnClickListener(v -> {
@@ -63,5 +78,16 @@ public class EventActivity extends AppCompatActivity{
                 Toast.makeText(EventActivity.this, "click...", Toast.LENGTH_SHORT).show();
                 break;
         }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        super.onTouchEvent(event);
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                Log.d("EventActivity", "---onTouchEvent---");
+                break;
+        }
+        return false;
     }
 }
